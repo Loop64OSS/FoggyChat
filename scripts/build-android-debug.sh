690db874d4bb4e953c76c5e1a5ace261af60b7ba
Mateusz Burcon <mateusz0burcon@gmail.com>
2025-08-10 20:36:38 +0200

    Updates debug build script
    Updates the debug build script to use environment variables for project path, Android SDK path, and ADB debug device ID.
    This change improves portability and configuration management by centralizing configuration in the build environment.

--- scripts/build-android-debug.sh
+++ scripts/build-android-debug.sh
@@ -1,3 +1,5 @@
-cd ~/FoggyChat/client-gui
+#!/bin/bash
+. ./scripts/build.env
+cd $PROJECT_PATH/client-gui
 cargo tauri android build --debug
-~/Android/Sdk/platform-tools/adb -s RFCX60K0DQX install ~/FoggyChat/client-gui/src-tauri/gen/android/app/build/outputs/apk/universal/debug/app-universal-debug.apk
+$ANDROID_HOME/platform-tools/adb -s $ADB_DEBUG_DEVICE_ID install $PROJECT_PATH/client-gui/src-tauri/gen/android/app/build/outputs/apk/universal/debug/app-universal-debug.apk
