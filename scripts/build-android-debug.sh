4c8d0ce6f64367110dfa3611431264cc6f05740e
Mateusz Burcon <mateuszburcon@mail.in.loop64.com>
2025-09-04 22:58:00 +0200

    fix: build script path correction

--- scripts/build-android-debug.sh
+++ scripts/build-android-debug.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 . ./scripts/build.env
-cd $PROJECT_PATH/client-gui
+cd $PROJECT_PATH
 cargo tauri android build --debug --verbose
-$ANDROID_HOME/platform-tools/adb -s $ADB_DEBUG_DEVICE_ID install $PROJECT_PATH/client-gui/src-tauri/gen/android/app/build/outputs/apk/universal/debug/app-universal-debug.apk
+$ANDROID_HOME/platform-tools/adb -s $ADB_DEBUG_DEVICE_ID install $PROJECT_PATH/src-tauri/gen/android/app/build/outputs/apk/universal/debug/app-universal-debug.apk
