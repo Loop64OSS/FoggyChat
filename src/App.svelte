42e6e429c452a60a03753e7a0324b1e6808055a8
Mateusz Burcon <mateusz0burcon@proton.me>
2026-02-19 01:14:34 +0100

    feat: notifications, fix: bug fixes

--- src/App.svelte
+++ src/App.svelte
@@ -3,7 +3,6 @@
     import { Router, Link, Route, navigate } from "svelte-routing";
     import MessagesPage from "./routes/MessagePage.svelte";
     import InitPage from "./routes/InitPage.svelte";
-    import { invoke } from "@tauri-apps/api/core";
     import { onDestroy, onMount, tick } from "svelte";
     import { listen } from "@tauri-apps/api/event";
     import { toaster } from "./lib/toaster-svelte";
@@ -12,6 +11,7 @@
     if (import.meta.env.MODE !== "development") {
         window.addEventListener("contextmenu", (e) => e.preventDefault());
     }
+
     let unlisten: () => void;
     onMount(async () => {
         if (!unlisten) {
